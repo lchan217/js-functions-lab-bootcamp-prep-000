ad5235104335814fc693a9ee1a275c3f63e837c7
Elizabeth Chan <lchan217@gmail.com>
2019-01-08 22:03:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ function happyHolidaysTo(name) {return `Happy holidays, ${name}!`
 }
 function happyHolidayTo(holiday, name){return `Happy ${holiday}, ${name}!`
 }
+function holidayCountdown(holiday, days){return 
